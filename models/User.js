3b6cbd09efe9fe579ea536efb8ecb033e3c2aae3
Cation-Coder6 <200516@kiit.ac.in>
2022-01-19 02:17:23 +0530

    Model and password hashing

--- models/User.js
+++ models/User.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     name: {
         type: "String",
         required: [true, "Please Provide Name"],
+        minlength: 3,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Please Provide Email"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please Provide a Valid Username",
+        ],
+        unique: true,
+    },
+    password: {
+        type: "String",
+        required: [true, "Please Provide Password"],
+        minlength: 6,
+    },
+});
 
-
-    }
-})
+module.exports = mongoose.model("User", UserSchema);
