d30d5634fa393caf5855b5e9a4141710b1225411
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-01-21 02:53:03 +0530

    Account Existance Based on Email and Method Use

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,29 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
 
 const register = async(req, res) => {
     const user = await User.create({...req.body });
-    res.status(StatusCodes.CREATED).json({ user });
+    const token = user.createJWT();
+    res
+        .status(StatusCodes.CREATED)
+        .json({ user: { name: user.getName() }, token });
 };
 
 const login = async(req, res) => {
-    res.send("Login user");
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError("Please provide Email and Password")
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+    const token = user.createJWT();
+    res
+        .status(StatusCodes.OK)
+        .json({ user: { name: user.name }, token })
 };
 
 module.exports = {
