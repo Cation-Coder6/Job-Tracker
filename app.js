f8971c3c907dff74ef4e1166b39286e3bf2bd77e
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-01-22 21:04:19 +0530

    Security Added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//Extra Security Pakages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit')
+
+
 const express = require('express');
 const app = express();
+
 
 const authenticateUser = require('./middleware/authentication')
 
@@ -18,7 +27,17 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set("trust proxy", 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, //15 minutes
+        max: 100, //limit ip to 100 requests per windowMs
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
